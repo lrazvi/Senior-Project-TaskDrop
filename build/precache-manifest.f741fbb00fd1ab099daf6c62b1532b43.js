c9180a36c89ac85ded5cc1a58b356ae8222c159e
lrazvi <lrazvi@oxy.edu>
2023-02-16 03:41:31 -0800

    build

---
+++ build/precache-manifest.f741fbb00fd1ab099daf6c62b1532b43.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a7117759097df02b7c",
+    "url": "/comps/static/css/main.0cc6a241.chunk.css"
+  },
+  {
+    "revision": "21a7117759097df02b7c",
+    "url": "/comps/static/js/main.21a71177.chunk.js"
+  },
+  {
+    "revision": "f33533a4a085a58163e6",
+    "url": "/comps/static/js/1.f33533a4.chunk.js"
+  },
+  {
+    "revision": "04786945b900032ebeaf",
+    "url": "/comps/static/js/runtime~main.04786945.js"
+  },
+  {
+    "revision": "b439c7371136ee50b932e753e1b49b29",
+    "url": "/comps/static/media/ghost_000.b439c737.png"
+  },
+  {
+    "revision": "0da2005c5b588a23fe9d1d81e790da3c",
+    "url": "/comps/static/media/sfx-animal-cat3.0da2005c.mp3"
+  },
+  {
+    "revision": "2c405a25ff2a9c46712886b69435cb4f",
+    "url": "/comps/static/media/mixkit-arcade-game-jump-coin-216.2c405a25.wav"
+  },
+  {
+    "revision": "55e9da172a42e4ec2127ce073a9688ce",
+    "url": "/comps/static/media/mixkit-bonus-earned-in-video-game-2058.55e9da17.wav"
+  },
+  {
+    "revision": "24d24e68c3508cf9eb243cc76c59171e",
+    "url": "/comps/index.html"
+  }
+];
